d323afa1b300bb8f68b02282cccf678c6233d5c7
Anton Afanasyev <fun@bk.ru>
2016-11-26 12:19:32 +0000

    Draft for postgres model

---
+++ model/postgres/postgres.go
@@ -0,0 +1,286 @@
+package postgres
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+
+	dbot "github.com/depechebot/depechebot"
+)
+
+type Model struct {
+	db *sql.DB
+	//tableName string
+	//chats []*dbot.Chat
+}
+
+func NewModel(db *sql.DB) Model {
+	return Model{db: db}
+}
+
+// Init initializes model.
+// num is the number of existing chats.
+func (m Model) Init() (chatIDs []dbot.ChatID, err error) {
+
+	err = m.db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.createTable()
+	if err != nil {
+		return nil, err
+	}
+
+	const sqlstr = `SELECT chat_id from ` + `chat`
+	q, err := m.db.Query(sqlstr)
+	if err != nil {
+		return nil, err
+	}
+	defer q.Close()
+
+	var chatID dbot.ChatID
+	chatIDs = []dbot.ChatID{}
+	for q.Next() {
+		err = q.Scan(&chatID)
+		if err != nil {
+			return nil, err
+		}
+
+		chatIDs = append(chatIDs, chatID)
+	}
+
+	return chatIDs, nil
+}
+
+func (m Model) createTable() error {
+	var err error
+
+	const sqlstr = `CREATE TABLE IF NOT EXISTS ` +
+		`chat` +
+		` (
+  primary_id INTEGER NOT NULL PRIMARY KEY,
+  chat_id BIGINT UNIQUE NOT NULL,
+  type TEXT NOT NULL,
+  abandoned BOOLEAN NOT NULL,
+  user_id INTEGER NOT NULL,
+  user_name TEXT NOT NULL DEFAULT '',
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  open_time TIMESTAMP NOT NULL,
+  last_time TIMESTAMP NOT NULL,
+  state TEXT NOT NULL,
+  params TEXT NOT NULL
+);
+`
+	_, err = m.db.Exec(sqlstr)
+
+	return err
+}
+
+// Exists determines if the Chat exists in the database.
+func (m Model) Exists(c *dbot.Chat) (exists bool, err error) {
+	var cnt int
+	var sqlstr = `SELECT count(*) as count from ` + `chat` + ` where chat_id = ?`
+	err = m.db.QueryRow(sqlstr, c.ChatID).Scan(&cnt)
+	return cnt != 0, err
+}
+
+// Insert inserts chat to the database.
+// Sets c.PrimaryID.
+func (m Model) Insert(c *dbot.Chat) error {
+	var err error
+
+	const sqlstr = `INSERT INTO chat (` +
+		`chat_id, type, abandoned, user_id, user_name, first_name, last_name, open_time, last_time, state, params` +
+		`) VALUES (` +
+		`$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11` +
+		`)`
+
+	state, err := json.Marshal(c.State)
+	if err != nil {
+		return err
+	}
+	params, err := json.Marshal(c.Params)
+	if err != nil {
+		return err
+	}
+
+	res, err := m.db.Exec(sqlstr, c.ChatID, c.Type, c.Abandoned, c.UserID, c.UserName,
+		c.FirstName, c.LastName, c.OpenTime, c.LastTime, string(state), string(params))
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	c.PrimaryID = int(id)
+
+	return nil
+}
+
+// Update updates the Chat in the database.
+func (m Model) Update(c *dbot.Chat) error {
+	var err error
+
+	const sqlstr = `UPDATE chat SET ` +
+		`primary_id = $1, type = $2, abandoned = $3, user_id = $4, user_name = $5, first_name = $6, last_name = $7, open_time = $8, last_time = $9, state = $10, params = $11` +
+		` WHERE chat_id = $12`
+
+	state, err := json.Marshal(c.State)
+	if err != nil {
+		return err
+	}
+
+	params, err := json.Marshal(c.Params)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.db.Exec(sqlstr, c.PrimaryID, c.Type, c.Abandoned, c.UserID, c.UserName,
+		c.FirstName, c.LastName, c.OpenTime, c.LastTime, string(state), string(params), c.ChatID)
+	return err
+}
+
+// Save saves the Chat to the database.
+// Prefer Update() if you know that chat exists.
+func (m Model) Save(c *dbot.Chat) error {
+	exists, err := m.Exists(c)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return m.Update(c)
+	}
+
+	return m.Insert(c)
+}
+
+// Delete deletes the Chat from the database.
+func (m Model) Delete(c *dbot.Chat) error {
+	var err error
+
+	const sqlstr = `DELETE FROM chat WHERE chat_id = $1`
+
+	_, err = m.db.Exec(sqlstr, c.ChatID)
+	return err
+}
+
+// ChatByPrimaryID retrieves a chat by primaryID.
+func (m Model) ChatByPrimaryID(primaryID int) (*dbot.Chat, error) {
+	var err error
+	var state, params string
+
+	const sqlstr = `SELECT ` +
+		`primary_id, chat_id, type, abandoned, user_id, user_name, first_name, last_name, open_time, last_time, state, params ` +
+		`FROM chat ` +
+		`WHERE primary_id = $1`
+
+	c := dbot.Chat{}
+	err = m.db.QueryRow(sqlstr, primaryID).Scan(&c.PrimaryID, &c.ChatID, &c.Type, &c.Abandoned, &c.UserID, &c.UserName,
+		&c.FirstName, &c.LastName, &c.OpenTime, &c.LastTime, &state, &params)
+	if err != nil {
+		return nil, err
+		if err == sql.ErrNoRows {
+			return nil, errors.New("chat not found (use Model.Exist() before if not sure)")
+		} else {
+			return nil, err
+		}
+	}
+
+	err = json.Unmarshal([]byte(state), &c.State)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(params), &c.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+// ChatByChatID retrieves a chat by chatID.
+func (m Model) ChatByChatID(chatID dbot.ChatID) (*dbot.Chat, error) {
+	var err error
+	var state, params string
+
+	const sqlstr = `SELECT ` +
+		`primary_id, chat_id, type, abandoned, user_id, user_name, first_name, last_name, open_time, last_time, state, params ` +
+		`FROM chat ` +
+		`WHERE chat_id = $1`
+
+	c := dbot.Chat{}
+	err = m.db.QueryRow(sqlstr, chatID).Scan(&c.PrimaryID, &c.ChatID, &c.Type, &c.Abandoned, &c.UserID, &c.UserName,
+		&c.FirstName, &c.LastName, &c.OpenTime, &c.LastTime, &state, &params)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("chat not found (use Model.Exist() before if not sure)")
+		} else {
+			return nil, err
+		}
+	}
+
+	err = json.Unmarshal([]byte(state), &c.State)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(params), &c.Params)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+// ChatsByParam retrieves chats with chat.Params matching param.
+func (m Model) ChatsByParam(param string) ([]*dbot.Chat, error) {
+	var err error
+	var state, params string
+
+	const sqlstr = `SELECT ` +
+		`primary_id, chat_id, type, abandoned, user_id, user_name, first_name, last_name, open_time, last_time, state, params ` +
+		`FROM chat ` +
+		`WHERE ` +
+		`params like "%" || $1 || "%"`
+
+	q, err := m.db.Query(sqlstr, param)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return []*dbot.Chat{}, nil
+		} else {
+			return nil, err
+		}
+	}
+	defer q.Close()
+
+	chats := []*dbot.Chat{}
+	for q.Next() {
+		c := dbot.Chat{}
+
+		err = q.Scan(&c.PrimaryID, &c.ChatID, &c.Type, &c.Abandoned, &c.UserID, &c.UserName,
+			&c.FirstName, &c.LastName, &c.OpenTime, &c.LastTime, &state, &params)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal([]byte(state), &c.State)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal([]byte(params), &c.Params)
+		if err != nil {
+			return nil, err
+		}
+
+		chats = append(chats, &c)
+	}
+
+	return chats, nil
+}
---
+++ model/postgres/postgres_test.go
@@ -0,0 +1,172 @@
+package postgres
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+
+	dbot "github.com/depechebot/depechebot"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestSqlite3ModelInit(t *testing.T) {
+	var m dbot.Model
+
+	db, err := sql.Open("sqlite3", "./test.sqlite3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	m = NewModel(db)
+	chatIDs, err := m.Init()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Number of loaded chats: %d", len(chatIDs))
+}
+
+func TestSqlite3ModelSaveRetrieveDelete(t *testing.T) {
+	var m dbot.Model
+
+	db, err := sql.Open("sqlite3", "./test2.sqlite3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	m = NewModel(db)
+	chatIDs, err := m.Init()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Number of loaded chats: %d", len(chatIDs))
+
+	params := dbot.Params(map[string]string{"foo": "bar", "noo": "bab"})
+	state := dbot.State{Name: "TEST", Params: params}
+	chat := &dbot.Chat{
+		ChatID:    88000111222,
+		Abandoned: false,
+		Type:      "private",
+		UserID:    123232,
+		UserName:  "username",
+		FirstName: "first_name",
+		LastName:  "Вава",
+		OpenTime:  time.Now(),
+		LastTime:  time.Now(),
+		State:     state,
+		Params:    params,
+	}
+
+	err = m.Save(chat)
+	if err != nil {
+		t.Error(err)
+	}
+
+	chat.ChatID = -1324
+	exists, err := m.Exists(chat)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if exists {
+		chat, err = m.ChatByChatID(chat.ChatID)
+		if err != nil {
+			t.Error(err)
+		}
+		chat.Abandoned = !chat.Abandoned
+	}
+
+	err = m.Save(chat)
+	if err != nil {
+		t.Error(err)
+	}
+
+	chat2, err := m.ChatByChatID(88000111222)
+	if err != nil {
+		t.Error(err)
+	}
+	if chat2.LastName != "Вава" {
+		t.Error("ChatByChatID() failed")
+	}
+	if chat2.State.Params["noo"] != "bab" {
+		t.Error("ChatByChatID() failed")
+	}
+
+	chat3, err := m.ChatByPrimaryID(chat.PrimaryID)
+	if err != nil {
+		t.Error(err)
+	}
+	if chat3.ChatID != -1324 {
+		t.Error("ChatByPrimaryID() failed")
+	}
+	if chat2.State.Params["noo"] != "bab" {
+		t.Error("ChatByPrimaryID() failed")
+	}
+
+	chats, err := m.ChatsByParam("noo")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(chats) != 2 {
+		t.Error("ChatsByParam() failed")
+	}
+
+	chats[1].LastTime = time.Now()
+	err = m.Save(chats[1])
+	if err != nil {
+		t.Error(err)
+	}
+
+	chatID := chats[0].ChatID
+	err = m.Delete(chats[0])
+	if err != nil {
+		t.Error(err)
+	}
+	_, err = m.ChatByChatID(chatID)
+	if err == nil {
+		t.Error("Deleted chat retrieved with no error!")
+	}
+
+}
+
+func TestSqlite3ModelStateParams(t *testing.T) {
+	var m dbot.Model
+
+	db, err := sql.Open("sqlite3", "./test3.sqlite3")
+	if err != nil {
+		t.Error(err)
+	}
+
+	m = NewModel(db)
+	chatIDs, err := m.Init()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Number of loaded chats: %d", len(chatIDs))
+
+	params := dbot.Params(map[string]string{"foo": "bar", "noo": "bab"})
+	state := dbot.State{Name: "TEST", Params: dbot.Params{}}
+	chat := &dbot.Chat{
+		ChatID:    88000111222,
+		Abandoned: false,
+		Type:      "private",
+		UserID:    123232,
+		UserName:  "username",
+		FirstName: "first_name",
+		LastName:  "Вава",
+		OpenTime:  time.Now(),
+		LastTime:  time.Now(),
+		State:     state,
+		Params:    params,
+	}
+
+	err = m.Save(chat)
+	if err != nil {
+		t.Error(err)
+	}
+
+	chat, err = m.ChatByChatID(chat.ChatID)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
